e0cf6fbad07b17bb604f0e3b85fb7b514df4d454
mehranalam <mehranalambeigi@yahoo.com>
2022-02-22 08:28:33 +0330

    add menu for dashboard fragment.

--- app/src/main/java/com/example/bardeal/DashboardFragment.java
+++ app/src/main/java/com/example/bardeal/DashboardFragment.java
@@ -2,12 +2,15 @@ package com.example.bardeal;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.PopupMenu;
 import android.widget.RadioGroup;
 import android.widget.TextView;
 
+import androidx.annotation.MenuRes;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
@@ -39,11 +42,18 @@ public class DashboardFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
+        menu = view.findViewById(R.id.menu_for_some_works);
         profileImage = view.findViewById(R.id.profileImage);
         name = view.findViewById(R.id.nameOfUser);
         dateOfJoinToBardeal = view.findViewById(R.id.dataOfJoin);
         user = FirebaseAuth.getInstance().getCurrentUser();
 
+        menu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // show menu
+            }
+        });
 
 //        UserProfileChangeRequest profileUpdate = new UserProfileChangeRequest.Builder()
 //                .setDisplayName("empty")
@@ -55,6 +65,36 @@ public class DashboardFragment extends Fragment {
 
 
     }
+
+    private void showMenu(View v , @MenuRes int menuRes){
+        PopupMenu popupMenu = new PopupMenu(getContext() ,v);
+        popupMenu.getMenuInflater().inflate(menuRes ,popupMenu.getMenu());
+
+        popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem menuItem) {
+                if (menuItem.getItemId() == R.id.change_name){
+                    // todo : change display name
+
+                    return true;
+                } else if (menuItem.getItemId() == R.id.change_image_prof){
+                    // todo : change display Photo
+
+                    return true;
+                } else if (menuItem.getItemId() == R.id.change_categories) {
+                    // todo : change categories selected
+
+                    return true;
+                }
+
+                return false;
+            }
+        });
+
+        popupMenu.show();
+    }
+
+
 
 //    @Override
 //    public void onStart() {
